b6c669203febb920f9ecc50a419861f7db49cc1d
Taiyou Katsumata <taiyousun@example.com>
2019-07-05 15:28:58 +0900

    20190705_commit_02
    details.html_5_<div class = align-self-center>

--- searchapp/urls.py
+++ searchapp/urls.py
@@ -5,8 +5,8 @@ from . import views
 
 # どのアプリのurlsなのか分かるようにapp_nameを使用する。
 app_name = 'searchapp'
-'''
+
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='details'),
+    path('',views.post_list,name='post_list'),
+#    path('', views.IndexView.as_view(), name='details'),
 ]
-'''
--- searchapp/views.py
+++ searchapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import GoodsTBL
+from .models import HighCategoryTBL
 # Create your views here.
+
+def post_list(request):
+    posts = 'a'
+    return render(request,'searchapp/details.html',{'posts':posts})
